ad12b26a6bf1dc88ba7036b9c8163bfa07363c17
Thauany Silva Pacheco <65237686+pthauany@users.noreply.github.com>
2022-12-27 20:55:42 -0300

    Resolucao do exercicio (v1), ainda com alguns erros

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,55 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// https://play.kotlinlang.org/#eyJ2ZXJzaW9uIjoiMS43LjIxIiwicGxhdGZvcm0iOiJqYXZhIiwiYXJncyI6IiIsIm5vbmVNYXJrZXJzIjp0cnVlLCJ0aGVtZSI6ImlkZWEiLCJjb2RlIjoiZW51bSBjbGFzcyBOaXZlbCB7IEJBU0lDTywgSU5URVJNRURJQVJJTywgQVZBTkNBRE8gfVxuXG5kYXRhIGNsYXNzIFVzdWFyaW8odmFsIG5vbWU6IFN0cmluZykge1xuICAgIGNvbXBhbmlvbiBvYmplY3Qge1xuXG4gICAgfVxufVxuXG5kYXRhIGNsYXNzIENvbnRldWRvRWR1Y2FjaW9uYWwodmFsIG5vbWU6IFN0cmluZywgdmFsIGR1cmFjYW86IEludCwgdmFsIG5pdmVsOiBOaXZlbClcblxuZGF0YSBjbGFzcyBGb3JtYWNhbyh2YXIgbm9tZTogU3RyaW5nLCB2YXIgY29udGV1ZG9zOiBMaXN0PENvbnRldWRvRWR1Y2FjaW9uYWw+KSB7XG4gICAgXG4gICAgY29tcGFuaW9uIG9iamVjdCB7XG5cbiAgICB9XG4gICAgXG4gICAgdmFsIGluc2NyaXRvcyA9IG11dGFibGVMaXN0T2Y8VXN1YXJpbz4oKVxuICAgIFxuICAgIGZ1biBtYXRyaWN1bGFyKHZhcmFyZyB1c3VhcmlvOiBVc3VhcmlvKSB7XG4gICAgICAgIHVzdWFyaW8uZm9yRWFjaHtpbnNjcml0b3MuYWRkKGl0KX1cbiAgICB9XG59XG5cbmZ1biBtYWluKCkge1xuXG4gICAgdmFsIG5pdmVsID0gTml2ZWwuQkFTSUNPXG5cdHZhbCBtZXNzYWdlTml2ZWwgPSB3aGVuIChuaXZlbCkge1xuICAgIFx0Tml2ZWwuQkFTSUNPIC0+IFwiQmFzaWNvXCJcblx0XHROaXZlbC5JTlRFUk1FRElBUklPIC0+IFwiSW50ZXJtZWRpYXJpb1wiXG5cdFx0Tml2ZWwuQVZBTkNBRE8gLT4gXCJBdmFuY2Fkb1wiXG4gICAgfVxuICAgIFxuICAgIHZhbCBjb250ZXVkb3M6IExpc3Q8Q29udGV1ZG9FZHVjYWNpb25hbD4gPSBsaXN0T2YoQ29udGV1ZG9FZHVjYWNpb25hbChcIkludHJvZHXDp8OjbyBhIEtvdGxpblwiLCAzLCBuaXZlbD1OaXZlbC5CQVNJQ08pLFxuICAgIFx0XHRcdFx0XHRcdFx0XHRcdFx0XHRcdCAgQ29udGV1ZG9FZHVjYWNpb25hbChcIkNvbnRyb2xlIGRlIEZsdXhvXCIsIDQsIG5pdmVsPU5pdmVsLklOVEVSTUVESUFSSU8pLFxuICAgIFx0XHRcdFx0XHRcdFx0XHRcdFx0XHRcdCAgQ29udGV1ZG9FZHVjYWNpb25hbChcIlByb2dyYW1hw6fDo28gT3JpZW50YWRhIGEgT2JqZXRvc1wiLCAxMCwgbml2ZWw9Tml2ZWwuQVZBTkNBRE8pLFxuICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgQ29udGV1ZG9FZHVjYWNpb25hbChcIkZ1bsOnw7Vlc1wiLCA0LCBuaXZlbD1OaXZlbC5BVkFOQ0FETylcbiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgKVxuXG4gICAgRm9ybWFjYW8obm9tZSA9IFwiS290bGluIEV4cGVyaWVuY2VcIiwgY29udGV1ZG9zKS5tYXRyaWN1bGFyKFVzdWFyaW8oXCJGbGF2aWFcIiksXG4gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIFVzdWFyaW8oXCJNYXJjb3NcIikpXG4gICAgXG4gICAgcHJpbnRsbihcIk5hIGZvcm1hw6fDo28gJHtGb3JtYWNhb30gdGVtb3Mgb3MgY29udGV1ZG9zOiBcIikgXG4gICAgcHJpbnRsbihcIiR7Y29udGV1ZG9zLm1hcHtpdC5ub21lfS5qb2luVG9TdHJpbmcoKX0gY29tIGEgZHVyYcOnw6NvIGRlICR7Y29udGV1ZG9zLm1hcHtpdC5kdXJhY2FvfX0gaG9yYXMgZSBkZSBuw612ZWwgJHtjb250ZXVkb3MubWFwe2l0Lm5pdmVsfX1cIilcbiAgICBwcmludGxuKFwiRSBvcyBhbHVub3MgJFVzdWFyaW8gbWF0cmljdWxhZG9zXCIpXG4gICAgXG4gICAgLypTYcOtZGE6XG4gICAgICpJbnRyb2R1Y2FvIGEgS290bGluIGNvbSBhIGR1cmHDp8OjbyBkZSAzIGhvcmFzIGUgZGUgbsOtdmVsIGLDoXNpY29cbiAgICAgQ29udHJvbGUgZGUgZmx1eG8gY29tIGEgZHVyYcOnw6NvIGRlIDQgaG9yYXMgZSBkZSBuw612ZWwgaW50ZXJtZWRpw6FyaW9cbiAgICAgcHJpbnRsbihcIiR7Y29udGV1ZG9zLm1hcHtpdC5ub21lWzBdfS5qb2luVG9TdHJpbmcoKX0gY29tIGEgZHVyYcOnw6NvIGRlICR7Y29udGV1ZG9zLm1hcHtpdC5kdXJhY2FvWzBdfX0gaG9yYXMgZSBkZSBuw612ZWwgJHtjb250ZXVkb3MubWFwe2l0Lm5pdmVsWzBdfX1cIilcbiAgICAgb3UgdXNhciAuZmlyc3QgZSAubGFzdFxuICAgICAqL1xuICAgIFxufSJ9
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String) {
+    companion object {
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>) {
+    
+    companion object {
+
+    }
+    
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        usuario.forEach{inscritos.add(it)}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val nivel = Nivel.BASICO
+	val messageNivel = when (nivel) {
+    	Nivel.BASICO -> "Basico"
+		Nivel.INTERMEDIARIO -> "Intermediario"
+		Nivel.AVANCADO -> "Avancado"
+    }
+    
+    val conteudos: List<ConteudoEducacional> = listOf(ConteudoEducacional("Introdução a Kotlin", 3, nivel=Nivel.BASICO),
+    												  ConteudoEducacional("Controle de Fluxo", 4, nivel=Nivel.INTERMEDIARIO),
+    												  ConteudoEducacional("Programação Orientada a Objetos", 10, nivel=Nivel.AVANCADO),
+                                                      ConteudoEducacional("Funções", 4, nivel=Nivel.AVANCADO)
+                                                     )
+
+    Formacao(nome = "Kotlin Experience", conteudos).matricular(Usuario("Flavia"),
+                                                        Usuario("Marcos"))
+    
+    println("Na formação ${Formacao} temos os conteudos: ") 
+    println("${conteudos.map{it.nome}.joinToString()} com a duração de ${conteudos.map{it.duracao}} horas e de nível ${conteudos.map{it.nivel}}")
+    println("E os alunos $Usuario matriculados")
+    
+    /*Saída:
+     *Introducao a Kotlin com a duração de 3 horas e de nível básico
+     Controle de fluxo com a duração de 4 horas e de nível intermediário
+     println("${conteudos.map{it.nome[0]}.joinToString()} com a duração de ${conteudos.map{it.duracao[0]}} horas e de nível ${conteudos.map{it.nivel[0]}}")
+     ou usar .first e .last
+     */
+    
 }
